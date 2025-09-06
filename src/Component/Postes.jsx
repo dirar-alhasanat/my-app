fb8165c30be669d3919769a0ebca6c2282cfe15d
dirar-alhasanat <dirar.alhasanat@gmail.com>
2025-09-07 02:36:16 +0300

    post react

---
+++ src/Component/Postes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+ import "./poste.css"
+  function Postes({title,explain,image}) { 
+    
+    return( 
+    <div className="div_"> 
+        <h3>{title}</h3> 
+        <p>{explain}</p> 
+        <img src={image}></img> 
+    </div> 
+    ) 
+    } 
+    export default Postes;
